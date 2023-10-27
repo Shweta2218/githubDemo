ed348a0dfed17db9e02cb7e693b5e94559fcbfa6
Shweta Gupta <Shweta Gupta@DESKTOP-4PLI7SL>
2023-10-27 14:56:09 +0530

    This is first commit

---
+++ orangeDemo/src/test/java/com/bjs/test/OrangeHrmDemoTest.java
@@ -0,0 +1,30 @@
+package com.bjs.test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import com.bjs.dev.Homepage;
+import com.bjs.dev.Login;
+
+
+public class OrangeHrmDemoTest {
+	@Test
+	public  void unknownsMethod() {
+		// TODO Auto-generated method stub
+		ChromeDriver d=new ChromeDriver();
+		d.manage().window().maximize();
+		d.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		d.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		Login l=new Login(d);
+		l.typeUsername();
+		l.typePassword();
+		l.clickSubmitButton();
+		Homepage h=new Homepage(d);
+		h.clickoInc();
+		
+
+	}
+
+}
